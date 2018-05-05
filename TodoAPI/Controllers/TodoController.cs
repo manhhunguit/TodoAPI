f4a9a5d402b9730c025df17200b90829c650ae72
Hung Manh Nguyen <hungpluralsight001@outlook.com.vn>
2018-05-05 16:02:37 +0700

    Implement API to get todo by id.

---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Integrate Auth0" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+    }
+}
